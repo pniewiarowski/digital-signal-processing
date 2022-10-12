86768a0aac2ebb2f20c11bdeb8d71813f5e9f9f4
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2022-10-12 14:48:20 +0200

    Finish Exercises 1.1

--- tools/hello.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-class Hello(object):
-    def __init__(self, message: str) -> None:
-        self.message: str = message
-
-    def __str__(self) -> str:
-        return f'{datetime.now()}:\t{self.message} and I fucking hate that!'
